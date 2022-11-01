251abff58da2d7d8b0686eb37b9e7e5b1fb74309
Alyssa Carr <acarr@nelson-atkins.org>
2022-11-01 16:19:33 -0500

    Tried to add a title. Made some text changes

--- README.EMPTY.R
+++ README.EMPTY.R
@@ -1,1 +1,6 @@
-I am adding some text here. I messed up the file name, but I am not changing it. 
+# Not sure I'm doing this right
+
+I am adding some text here. I messed up the file name, but I am not changing it 
+making some changes to my file 
+
+
